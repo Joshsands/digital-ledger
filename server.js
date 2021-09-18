0d4d09907e2791c1f2021355768f8523ead7cadf
Joshua Sands <josh.sands@mail.com>
2021-09-18 16:23:17 -0500

    adjusted server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const logger = require("morgan");
+
 const mongoose = require("mongoose");
 const compression = require("compression");
 
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
-
-app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +16,7 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false,
+//   useFindAndModify: false,
   useUnifiedTopology: true
 });
 
